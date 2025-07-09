c178ac7a4246e897ce7ffa0520881f1f0b91965b
Nguyễn Việt Quốc An <40015342+cloodei@users.noreply.github.com>
2025-07-10 00:37:11 +0700

    insane redis overhead wtf

--- src/users.rs
+++ src/users.rs
@@ -1,7 +1,36 @@
-use axum::{extract::Path, http::StatusCode, Json};
+use axum::{extract::Path, http::{header, StatusCode}, response::IntoResponse, Json};
+use redis::AsyncCommands;
 use crate::prelude::{AppState, User, UserPayload};
 
-pub async fn list_users(AppState(state): AppState) -> Json<Vec<User>> {
+/// postgres version
+// pub async fn list_users(AppState(state): AppState) -> impl IntoResponse {
+//     let rows = state.client.query(&state.l_users, &[]).await.unwrap();
+//     let users = rows.iter().map(|row| User {
+//         id: row.get(0),
+//         name: row.get(1),
+//         email: row.get(2),
+//         password: row.get(3),
+//         trang_thai: row.get(4),
+//         created_at: row.get(5),
+//         updated_at: row.get(6)
+//     }).collect::<Vec<User>>();
+    
+//     Json(users)
+// }
+
+pub async fn list_users(AppState(state): AppState) -> impl IntoResponse {
+    let mut conn = state.redis.get_multiplexed_async_connection().await.unwrap();
+    let cached = conn.get::<&'static str, String>("users:all").await;
+
+    match cached {
+        Ok(r) => {
+            return ([(header::CONTENT_TYPE, "application/json")], r);
+        },
+        Err(err) => {
+            println!("Error: {}", err.to_string());
+        }
+    }
+
     let rows = state.client.query(&state.l_users, &[]).await.unwrap();
     let users = rows.iter().map(|row| User {
         id: row.get(0),
@@ -11,12 +40,43 @@ pub async fn list_users(AppState(state): AppState) -> Json<Vec<User>> {
         trang_thai: row.get(4),
         created_at: row.get(5),
         updated_at: row.get(6)
-    }).collect();
+    }).collect::<Vec<User>>();
+
+    let users = serde_json::to_string(&users).unwrap();
+    conn.set_ex::<&str, &String, ()>("users:all", &users, 60 * 60).await.unwrap();
     
-    Json(users)
+    ([(header::CONTENT_TYPE, "application/json")], users)
 }
 
-pub async fn list_user(AppState(state): AppState, Path(id): Path<i32>) -> Json<User> {
+/// postgres version
+// pub async fn list_user(AppState(state): AppState, Path(id): Path<i32>) -> Json<User> {
+//     let row = state.client.query_one(&state.l_user, &[&id]).await.unwrap();
+//     let user = User {
+//         id: row.get(0),
+//         name: row.get(1),
+//         email: row.get(2),
+//         password: row.get(3),
+//         trang_thai: row.get(4),
+//         created_at: row.get(5),
+//         updated_at: row.get(6)
+//     };
+    
+//     Json(user)
+// }
+
+pub async fn list_user(AppState(state): AppState, Path(id): Path<i32>) -> impl IntoResponse {
+    let mut conn = state.redis.get_multiplexed_async_connection().await.unwrap();
+    let cached = conn.get::<String, String>(format!("user:{}", id)).await;
+
+    match cached {
+        Ok(r) => {
+            return ([(header::CONTENT_TYPE, "application/json")], r);
+        },
+        Err(err) => {
+            println!("Error: {}", err.to_string());
+        }
+    }
+
     let row = state.client.query_one(&state.l_user, &[&id]).await.unwrap();
     let user = User {
         id: row.get(0),
@@ -28,7 +88,10 @@ pub async fn list_user(AppState(state): AppState, Path(id): Path<i32>) -> Json<U
         updated_at: row.get(6)
     };
     
-    Json(user)
+    let user = serde_json::to_string(&user).unwrap();
+    conn.set_ex::<String, &String, ()>(format!("user:{}", id), &user, 60 * 60).await.unwrap();
+    
+    ([(header::CONTENT_TYPE, "application/json")], user)
 }
 
 pub async fn insert_user(AppState(state): AppState, Json(payload): Json<UserPayload>) -> StatusCode {
